f2fe06bc5380f7d70e07a31db9c2517e2417e669
Noah Ross <noah.baine@gmail.com>
2018-12-04 19:34:59 -0800

    final updates to classic arcade game clone project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # udacity-nd001-p3
 Project 3: Classic Arcade Game for Udacity Nano-degree Front End Web Developer
+
+## Rules
+- Avoid the bugs
+- Move the player up to the water to Win
+- Use the arrow pad on keyboard to control the character
+
+## Development Notes
+- Rewrote starter code in [app.js](../js/app.js) to favor Class style syntax
+- Font used is the [Press Start 2P](https://fonts.google.com/specimen/Press+Start+2P?selection.family=Press+Start+2P) font offered by Google Fonts
--- js/app.js
+++ js/app.js
@@ -1,98 +1,94 @@
-// Enemies our player must avoid
-// var Enemy = function() {
-//     // Variables applied to each of our instances go here,
-//     // we've provided one for you to get started
-
-//     // The image/sprite for our enemies, this uses
-//     // a helper we've provided to easily load images
-//     this.sprite = 'images/enemy-bug.png';
-// };
-
-
-let wins = 0;
-let losses = 0;
-
+// I got rid of starter code and converted to class syntax
 
 class Enemy {
     constructor() {
+        this.spawn();
         this.sprite = "images/enemy-bug.png";
     }
-
     update(dt) {
-
+        this.x += this.sp * dt;
+        if (this.x > 505) {
+            this.spawn();
+        }
+        player.collision(this);
+    }
+    spawn() { // this method generates an enemy based on a random clamped Y and static off screen X with a variable speed
+        this.x = -100;
+        this.y = enemyLookupY[Math.floor(Math.random() * enemyLookupY.length)];
+        this.sp = Math.floor(100 + (Math.random() * 300));
     }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-// Enemy.prototype.update = function(dt) {
-//     // You should multiply any movement by the dt parameter
-//     // which will ensure the game runs at the same speed for
-//     // all computers.
-// };
-
-// Draw the enemy on the screen, required method for game
-// Enemy.prototype.render = function() {
-//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-// };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
 class Player {
-
     constructor(x, y, sp) {
         this.x = x;
         this.y = y;
-        this.sp = sp;
         this.sprite = "images/char-boy.png";
     }
-
     update(dt) {
-        this.y > 400 ? this.y = 400 : 0 ;
-        this.x > 400 ? this.x = 400 : 0 ;
-        this.x < 0 ? this.x = 0 : 0 ;
+        this.y > 400 ? this.y = 400 : 0;
+        this.x > 400 ? this.x = 400 : 0;
+        this.x < 0 ? this.x = 0 : 0;
         this.y < 0 ? this.win() : 0;
     }
-
-    win(){
-        console.log('won');
+    win() { // win condition method to increment win counter and update win text and  then reset the player
         wins++;
+        document.getElementById("wins").textContent = `Wins: ${wins}`;
         this.y = 400;
     }
-
+    collision(badguy) {
+        // this method checks a variance of +- 70 pixels of the players X
+        // value and the enemy X value while making sure they are on the same Y value.
+        // If collision is found - death counter is incremented, UI updated, and player is reset.
+        if (badguy.y == player.y && badguy.x + 70 > player.x && badguy.x - 70 < player.x) {
+            player.reset();
+        }
+    }
+    reset() { //resets player location to starting position
+        this.x = 200;
+        this.y = 400;
+        losses++;
+        document.getElementById("losses").textContent = `Deaths: ${losses}`;
+    }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-    handleInput(key) {
+    handleInput(key) { // handles key inputs and moves player position accordingly
         switch (key) {
             case 'up':
-                this.y -= this.sp + 35;
+                this.y -= 85;
                 break;
             case 'down':
-                this.y += this.sp + 35;
+                this.y += 85;
                 break;
             case 'left':
-                this.x -= this.sp + 55;
+                this.x -= 100;
                 break;
             case 'right':
-                this.x += this.sp + 55;
+                this.x += 100;
                 break;
         }
     }
 }
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+// init variables
+const enemyLookupY = [60, 145, 230];
+let wins = 0;
+let losses = 0;
 let allEnemies = [];
-let player = new Player(200, 400, 50);
+let player = new Player(200, 400);
 
+// build enemies
+for (var i = 0; i < 5; i++) {
+    allEnemies.push(new Enemy());
+}
+
+// update UI text on start
+document.getElementById("game-title").textContent = `Death by Ladybug`;
+document.getElementById("wins").textContent = `Wins: ${wins}`;
+document.getElementById("losses").textContent = `Deaths: ${losses}`;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -103,6 +99,5 @@ document.addEventListener('keyup', function (e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
